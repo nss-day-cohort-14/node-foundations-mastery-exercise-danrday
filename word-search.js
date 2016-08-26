95bcadb64d4e6fb2afc0b81a800dbb7fbdf6a3df
Dan Day <danday@gmail.com>
2016-08-26 09:43:41 -0500

    read and print word file

--- word-search.js
+++ word-search.js
@@ -1,1 +1,9 @@
-console.log("hey")
+#!/usr/bin/env node
+
+const [,, ...args] = process.argv
+
+var fs = require("fs");
+
+var readStream = fs.createReadStream('/usr/share/dict/words')
+
+readStream.pipe(process.stdout)
