48af2e86343c1cc126fd4fbe0ba2676bcb8458f8
Dan Day <danday@gmail.com>
2016-08-28 17:53:36 -0500

    delete unused code

--- hackerTyper.js
+++ hackerTyper.js
@@ -1,14 +1,36 @@
+
 const { Transform } = require('stream')
 
 const hackerTyper = Transform()
 
+
 hackerTyper._transform = (buffer, _, cb) => {
 
-  function f() {
-    cb(null, buffer)
+
+  let word = buffer.toString().split('')
+
+
+  function* generator(word){
+
+  for (i=0; i<word.length; i++) {
+    yield word[i]
   }
-  setTimeout(f, 50)
+}
+
+  var gen = generator(word);
+
+  for (i=0; i<word.length; i++) {
+    console.log("val", gen.next().value)
+  }
+
+
+  // console.log("val", gen.next().value)
+
+
+  cb(null, word[0])
 
 }
+
+
 
 module.exports = { hackerTyper }
--- word-search.js
+++ word-search.js
@@ -1,24 +1,14 @@
 #!/usr/bin/env node
 
 const [,, ...args] = process.argv
-//
+
 var fs = require("fs");
-//
+
 var es = require('event-stream')
-//
 
 const { limitTen } = require('./limit-ten')
 
 const { hackerTyper } = require('./hackerTyper')
-
-////// /usr/share/dict/words
-
-// process.stdin.resume();
-
-// process.stdin.setEncoding('utf8');
-
-// var readStream = fs.createReadStream(fileToRead)
-
 
 process.stdin.once('readable', function (pipedArgument) {
 
@@ -39,72 +29,8 @@ process.stdin.once('readable', function (pipedArgument) {
         cb(null, newData)
 
       }))
-      .pipe(limitTen).pipe(hackerTyper).pipe(process.stdout)
+      .pipe(limitTen)
+      // .pipe(hackerTyper)
+      .pipe(process.stdout)
 
 });
-
-////////
-
-
-
-
-
-// data = '';
-//
-//   process.stdin.setEncoding("utf-8");
-//
-//   process.stdin.on('readable', function() {
-//     var chunk;
-//     while (chunk = process.stdin.read()) {
-//       data += chunk;
-//     }
-//
-//     data.pipe(process.stdout)
-//
-//   });
-
-
-
-
-//////
-// process.stdin.resume();
-//
-// process.stdin.setEncoding('utf8');
-//
-// process.stdin.on('data', function (chunk) {
-//   // passedArgs += chunk
-//   process.stdout.write('Data! -> ' + chunk);
-// });
-//
-// // var data = fs.readFileSync(process.stdin);
-//
-// // console.log("data:", data)
-//
-// process.stdin.on('end', function() {
-//   process.stderr.write('End\n')
-// })
-
-
-////////
-
-//
-// const { limitTen } = require('./limit-ten')
-//
-// wordToSearchFor = args[0].toLowerCase()
-//
-// readStream.pipe(es.split()).pipe(es.map(function (data, cb) {
-//
-//   let newData = "no match found\n"
-//
-//       currentComparedWord = data.toLowerCase();
-//
-//       var n = currentComparedWord.indexOf(wordToSearchFor);
-//
-//       if ( n === 0 ) {
-//         newData = data + '\n'
-//       }
-//
-//       cb(null, newData)
-//
-//     }))
-//     .pipe(limitTen).pipe(process.stdout)
