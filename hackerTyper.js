091f20e95a37aae08ef2160a84733211041cc63d
Dan Day <danday@gmail.com>
2016-08-31 18:12:11 -0500

    add comments

--- hackerTyper.js
+++ hackerTyper.js
@@ -3,16 +3,19 @@ const { Transform } = require('stream')
 
 const hackerTyper = Transform()
 
+//ms
 letterInterval = 50
 
 hackerTyper._transform = (buffer, _, cb) => {
 
   const letters = [...buffer.toString()]
 
+  //pushes each letter by 50ms * letter index
   letters.forEach((letter, i) => {
     setTimeout( () => hackerTyper.push(letters[i]), letterInterval*i)
   })
 
+  //empty callback after wordlength * 50ms
   setTimeout( () => cb(), buffer.length*letterInterval);
 
 }
--- word-search.js
+++ word-search.js
@@ -10,10 +10,12 @@ const { limitTen } = require('./limit-ten')
 
 const { hackerTyper } = require('./hackerTyper')
 
+// pipes in argument through terminal, e.g. cat /usr/share/dict/words | word-search
 process.stdin.once('readable', function (pipedArgument) {
 
   wordToSearchFor = args[0].toLowerCase()
 
+  //es.split() splits by word, then es.map() pipes individual words through
   process.stdin.pipe(es.split()).pipe(es.map(function (data, cb) {
 
     let newData = "no match found\n"
@@ -22,16 +24,15 @@ process.stdin.once('readable', function (pipedArgument) {
 
         var n = currentComparedWord.indexOf(wordToSearchFor);
 
+        //if match, pipe new word through
         if ( n === 0 ) {
           newData = data + '\n'
           cb(null, newData)
         }
+        //if no match found, empty callback
         else {
           cb()
         }
-
-
-
 
       }))
       .pipe(limitTen)
